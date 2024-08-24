9fe48e3b935bfc5921400353962591686b83762e
Kamal Chanchal <40321363+Coderixc@users.noreply.github.com>
2024-08-24 18:55:36 +0530

    Lab 2 : Basics of R from lIst to Vector (24 Aug 24)

--- Lab_R_1.R
+++ Lab_R_1.R
@@ -54,7 +54,59 @@ num+num2
 num <- 5
 text<- 'concate'
 str(num)+text
-#ERRROR : If you try to combine a string (text) and a number, R will give you an error:
+#ERRROR : If you try to combine a string (text) and a number, R will give you an error
+
+#R Multiple Variables
+#assing the same values
+var1 <- var2 <- var3 <- "Orange"
+var1
+var2
+var3
+
+#Variable Name
+.myvar <- "John" #Legal Name
+
+#Basic Data Type in R - class
+#numeric
+x <- 10L #L denote Integer
+class(x)
+
+y<- 10.5
+class(y)
+
+#comples
+x<- -9i +3
+class(x)
+
+#logical (Bollean)
+x<- TRUE
+class(x)
+
+
+#Numbers
+
+x1 <- 10.5
+x2 <- 10L
+x3<- 1i 
+
+#Numeric - FLOAT or REAL NUMBER
+
+#TYPE CONVESRION
+#as.numeric()
+#as.integer()
+#as.complex()
+y<- 10L
+as.numeric(y)
+
+qyt <- 50L
+int_qty <- as.integer(qyt)
+int_qty
+
+
+
+
+
+
 
 
 #TASK 2 : LOOPING
@@ -63,6 +115,54 @@ for (x in 2:100)
 {
   print(x)
 }
+
+
+#LAB 2
+
+name <- readline()
+age <- readline()
+check1 <- class(name)
+print(check1)
+check2 <- class(age)
+print(check2)
+
+#Vector
+v <- c(23,14,45,12,12,12)
+print(v)
+
+#paste and Paste 0
+paste(1,"2",3,4,"ram")
+paste0(1,"2",3,4,"ram")
+
+
+#second element
+secMax = v[2]
+print(secMax)
+
+#length
+length(v)
+sort(v)
+sum(v)
+prod(v)
+
+
+#remove
+v[-1]
+
+#unname(v)  --- remove the name or dimnames
+unname(v)
+help(unname)
+
+#Dollar Sign $
+#
+
+
+#;list
+lit1 <- list(1:7) 
+lit2 <- list(5:7)
+lit3 <- list(7:8)
+litU <- list(lit1,lit2,lit3)
+litU
 
 
 
